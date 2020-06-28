d5c8f4340fb1fa6dba3a3407f46663116ccf7d96
Et3rnauta <guidocanevello@hotmail.com>
2020-06-28 19:02:54 -0300

    Refactor frontend.js

--- public/frontend.js
+++ public/frontend.js
@@ -1,74 +1,74 @@
 $(function() {
     "use strict";
-    // for better performance - to avoid searching in DOM
     var content = $('#content');
     var input = $('#input');
     var status = $('#status');
-    // my color assigned by the server
+
+    var webSocketsServerIp = "localhost";
+    var webSocketsServerPort = 1337;
+
     var myColor = false;
-    // my name sent to the server
     var myName = false;
-    // if user is running mozilla then use it's built-in WebSocket
+    // si el usuario entra desde mozilla, usar su propio web socket
     window.WebSocket = window.WebSocket || window.MozWebSocket;
-    // if browser doesn't support WebSocket, just show
-    // some notification and exit
+
+    // si no se da soporte a web socket
     if (!window.WebSocket) {
-        content.html($('<p>', { text: 'Sorry, but your browser doesn\'t support WebSocket.' }));
+        content.html($('<p>', { text: 'Tu navegador no soporta Web Socket.' }));
         input.hide();
         $('span').hide();
         return;
     }
-    // open connection
-    var connection = new WebSocket('ws://127.0.0.1:1337');
+
+    // abrir la conexión
+    var connection = new WebSocket(`ws://${webSocketsServerIp}:${webSocketsServerPort}`);
     connection.onopen = function() {
-        // first we want users to enter their names
+        // primero se solicita el nombre
         input.removeAttr('disabled');
-        status.text('Choose name:');
+        status.text('Elegir Nombre:');
     };
     connection.onerror = function(error) {
-        // just in there were some problems with connection...
+        // si hay problemas de conexión
         content.html($('<p>', {
-            text: 'Sorry, but there\'s some problem with your ' +
-                'connection or the server is down.'
+            text: 'Problemas en la conexión con el servidor'
         }));
     };
-    // most important part - incoming messages
+
+    /**
+     * Recepcion de mensajes
+     */
     connection.onmessage = function(message) {
-        // try to parse JSON message. Because we know that the server
-        // always returns JSON this should work without any problem but
-        // we should make sure that the massage is not chunked or
-        // otherwise damaged.
         try {
+            //se intenta parsear el mensaje
             var json = JSON.parse(message.data);
         } catch (e) {
             console.log('Invalid JSON: ', message.data);
             return;
         }
-        // NOTE: if you're not sure about the JSON structure
-        // check the server source code above
-        // first response from the server with user's color
+
         if (json.type === 'color') {
+            // primer respuesta es el color
             myColor = json.data;
             status.text(myName + ': ').css('color', myColor);
             input.removeAttr('disabled').focus();
-            // from now user can start sending messages
-        } else if (json.type === 'history') { // entire message history
-            // insert every single message to the chat window
+        } else if (json.type === 'history') {
+            // ingresar todos los mensajes del historial en la ventana
             for (var i = 0; i < json.data.length; i++) {
                 addMessage(json.data[i].author, json.data[i].text,
                     json.data[i].color, new Date(json.data[i].time));
             }
-        } else if (json.type === 'message') { // it's a single message
-            // let the user write another message
+        } else if (json.type === 'message') {
+            // recibir mensaje y permitir escribir otro
             input.removeAttr('disabled');
             addMessage(json.data.author, json.data.text,
                 json.data.color, new Date(json.data.time));
         } else {
-            console.log('Hmm..., I\'ve never seen JSON like this:', json);
+            console.log('Formato de JSON no reconocido:', json);
         }
     };
+
     /**
-     * Send message when user presses Enter key
+     * Enviar un mensaje cuando se presiona la tecla Enter
      */
     input.keydown(function(e) {
         if (e.keyCode === 13) {
@@ -76,32 +76,21 @@ $(function() {
             if (!msg) {
                 return;
             }
-            // send the message as an ordinary text
             connection.send(msg);
             $(this).val('');
-            // disable the input field to make the user wait until server
-            // sends back response
+
+            // espero hasta que el servidor responde
             input.attr('disabled', 'disabled');
-            // we know that the first message sent from a user their name
+
+            // como el nombre es el primer mensaje, lo guardo
             if (myName === false) {
                 myName = msg;
             }
         }
     });
+
     /**
-     * This method is optional. If the server wasn't able to
-     * respond to the in 3 seconds then show some error message 
-     * to notify the user that something is wrong.
-     */
-    setInterval(function() {
-        if (connection.readyState !== 1) {
-            status.text('Error');
-            input.attr('disabled', 'disabled').val(
-                'Unable to communicate with the WebSocket server.');
-        }
-    }, 3000);
-    /**
-     * Add message to the chat window
+     * Agregar un mensaje a la ventana de chat
      */
     function addMessage(author, message, color, dt) {
         content.prepend('<p><span style="color:' + color + '">' +
